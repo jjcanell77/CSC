c3f78b3e0feeabba6fd595e6365487621d5de667
jjcanell77 <jjcanell@yahoo.com>
2023-10-29 21:55:11 -0500

    Create PersonInfo.java

---
+++ PersonInfo.java
@@ -0,0 +1,16 @@
+public class PersonInfo {
+    public static void main(String[] args) {
+        String firstName = "Jesse";
+        String lastName = "Canell";
+        String streetAddress = "123 Elm Street";
+        String city = "Springfield";
+        String zipCode = "12345";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
+
