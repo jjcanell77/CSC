3787e8b727a29318cb6f713b30751aa9ac9cf6ed
jjcanell77 <jjcanell@yahoo.com>
2024-05-01 14:57:19 -0500

    Added Sierpinski Gasket function

--- CSC405/M2/index.js
+++ CSC405/M2/index.js
@@ -3,26 +3,66 @@ const createShader = require('gl-shader')
 const createBuffer = require('gl-buffer')
 const mat4 = require('gl-mat4')
 
+const initialVertices = [
+  0.0, 0.5, 0.0,
+  -0.5, -0.5, 0.0,
+  0.5, -0.5, 0.0 
+];
+const depth = 6;// can adjust the loop as needed
+
+function divideTriangle(vertices, depth) {
+  let triangles = [vertices];
+
+  for (let d = 0; d < depth; d++) {
+    let newTriangles = [];
+    for (let i = 0; i < triangles.length; i++) {
+      const currentTriangle = triangles[i];
+
+      // calculates midpoints for each side
+      const mid1 = [(currentTriangle[0] + currentTriangle[3]) / 2, (currentTriangle[1] + currentTriangle[4]) / 2, (currentTriangle[2] + currentTriangle[5]) / 2];
+      const mid2 = [(currentTriangle[3] + currentTriangle[6]) / 2, (currentTriangle[4] + currentTriangle[7]) / 2, (currentTriangle[5] + currentTriangle[8]) / 2];
+      const mid3 = [(currentTriangle[6] + currentTriangle[0]) / 2, (currentTriangle[7] + currentTriangle[1]) / 2, (currentTriangle[8] + currentTriangle[2]) / 2];
+
+      // creates new triangles from the midpoints
+      newTriangles.push([
+          currentTriangle[0], currentTriangle[1], currentTriangle[2],
+          mid1[0], mid1[1], mid1[2],
+          mid3[0], mid3[1], mid3[2]
+      ]);
+      newTriangles.push([
+          mid1[0], mid1[1], mid1[2],
+          currentTriangle[3], currentTriangle[4], currentTriangle[5],
+          mid2[0], mid2[1], mid2[2]
+      ]);
+      newTriangles.push([
+          mid3[0], mid3[1], mid3[2],
+          mid2[0], mid2[1], mid2[2],
+          currentTriangle[6], currentTriangle[7], currentTriangle[8]
+      ]);
+    }
+    triangles = newTriangles;
+  }
+  // makes the multiples arrays of traingles into a single array
+  return triangles.flat();
+}
+
+
 let shader, buffer
 shell.on('gl-init', function () {
   const gl = shell.gl
+  subdividedVertices = divideTriangle(initialVertices, depth); 
+
   shader = createShader(gl, `
-    uniform mat4 model;
-    uniform mat4 camera;
     attribute vec3 position;
     void main() {
-      gl_Position = camera * model * vec4(position, 1.0);
+      gl_Position =  vec4(position, 1.0);
     }
   `, `
     void main() {
-      gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
     }
   `)
-  buffer = createBuffer(gl, [
-    0.0, 0.5, 0.0,
-    -0.5, -0.5, 0.0,
-    0.5, -0.5, 0.0,
-  ])
+  buffer = createBuffer(gl, subdividedVertices);
 })
 
 shell.on('gl-render', function () {
@@ -30,18 +70,6 @@ shell.on('gl-render', function () {
   shader.bind()
   buffer.bind()
 
-  const camera = mat4.create()
-  mat4.perspective(camera, 45 * Math.PI / 180, shell.width / shell.height, 0.1, 1000.0)
-  mat4.translate(camera, camera, [0, 0, -2])
-  shader.uniforms.camera = camera
-
-  const model = mat4.create()
-  mat4.translate(model, model, [-.3, 0, 0])
-  //mat4.rotate(model, model, 45, [0, 0, 1])
-  mat4.scale(model, model, [.5, .5, 1])
-  shader.uniforms.model = model
   shader.attributes.position.pointer()
-  gl.drawArrays(gl.TRIANGLES, 0, 3)
+  gl.drawArrays(gl.TRIANGLES, 0, subdividedVertices.length / 3); 
 })
-
-
