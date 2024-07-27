a755c1553251f854427a61e8fca1876ebe390c7a
Jesse James Canell Jr <119881707+jjcanell77@users.noreply.github.com>
2024-07-27 15:42:56 -0500

    Added M6 Discussion

--- CSC450/M6/Discussion.java
+++ CSC450/M6/Discussion.java
@@ -2,8 +2,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
-public class SecureApp {
+public class Discussion {
      public static void main(String[] args) {
           String query = "SELECT * FROM users WHERE username = ? AND password = ?";
           String url = "jdbc:mysql://localhost:3306/mydatabase";
@@ -13,7 +14,7 @@ public class SecureApp {
           System.out.println("Enter password");
           String password = myObj.nextLine();
 
-        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
             PreparedStatement preparedStatement = conn.prepareStatement(query);
             preparedStatement.setString(1, "username");
             preparedStatement.setString(2, "securePassword");
